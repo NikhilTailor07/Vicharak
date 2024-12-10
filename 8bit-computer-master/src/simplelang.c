82176cbf3d73a48241423dbb5bf2d21832582483
Yogesh Tak <123325736+NikhilTailor07@users.noreply.github.com>
2024-12-10 12:31:02 +0530

    Add files via upload

---
+++ 8bit-computer-master/src/simplelang.c
@@ -0,0 +1,43 @@
+//   Design of Simplelang : A simple High - Level Language(in C language)
+// Variable declarations
+int a;
+int b;
+int c;
+int d;
+
+// Assignments
+a = 10;
+b = 20;
+c = a + b;
+d = c - b;
+
+// Conditional
+if (c == 30)
+{
+    d = d + 1;
+}
+
+/* Variable Declartions:
+int <variable_name>;
+*/
+
+/*Assignments:
+<variable_name> = <expression>;
+*/
+
+/*Conditionals:
+if (<condition>) {
+    <statements>;
+}
+
+== for equality
+!= for inequality
+< for less than
+> for greater than
+<= for less than or equal to
+>= for greater than or equal to
+*/
+
+/* Arithmetic Operations:
+<operand1> <operator> <operand2>;
+*/
